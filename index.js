592b36cd52a97bddec27412a6736f89094956185
Laurent Louf <laurentlouf@gmail.com>
2018-05-18 00:58:48 +0200

    Authentication done.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const puppeteer = require('puppeteer');
 
 (async () => 
 {
+  var loggedIn = false ;
+  var login = '', password = '' ;
+
   // Launch browser in slow mo, create page with desktop viewport and log console outputs
   const browser = await puppeteer.launch({slowMo: 1000});
   const page = await browser.newPage();
@@ -9,8 +12,30 @@ const puppeteer = require('puppeteer');
   page.on('console', msg => console.log('PAGE LOG:', msg.text() ) );
   
   // Go to the website
-  await page.goto('https://teleservices.paris.fr/dansmarue/');
-  await page.screenshot({path: 'example.png'});
+  try 
+  {
+    await page.goto('https://moncompte.paris.fr/moncompte/jsp/site/Portal.jsp?page=myluteceusergu&view=createAccountModal') ;
+    
+    // Create a promise to wait for execution first : if done after the click, the page may already have changed ("navigation" finished) before the promise creation, which means we would wait for another "navigation" that will never occur, since we won't navigate further after login
+    var promiseLogin = page.waitForNavigation({timeout: 10000}) ; 
+    await page.type('#username-login', login) ;
+    await page.type('#password-login', password) ;     
+    await page.click('button[name="Submit"]') ; 
+    await promiseLogin ; 
+
+    // If it loaded, it means the authentication went well
+    loggedIn = true ; 
+
+  } catch ( e )
+  {
+    console.log("A promise has been rejected. It may mean that the login information are wrong if it has been rejected because of a timeout. See the message below and screenshot of the page in the file promiseRejected.png.\n" + e) ; 
+    await page.screenshot({path: 'promiseRejected.png'});
+  }
+
+  if ( loggedIn )
+  {
+    await page.goto('https://teleservices.paris.fr/dansmarue/');
+  }
 
   // Close everything  
   await browser.close();
